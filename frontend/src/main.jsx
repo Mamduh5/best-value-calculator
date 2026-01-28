cfecb44d434732c55a420eb99b18a0fb5839d54c
Mamduh <mamduh2542@gmail.com>
2026-01-28 16:39:57 +0700

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { registerSW } from "virtual:pwa-register";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  registerSW({
+  </StrictMode>
+);
+
+// ✅ register service worker OUTSIDE React
+registerSW({
   onNeedRefresh() {},
   onOfflineReady() {
     console.log("App ready to work offline");
   },
-})
-)
+});
